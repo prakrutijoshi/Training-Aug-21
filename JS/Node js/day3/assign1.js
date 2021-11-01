8655901e0c1378fdf0cb4b338a0aae602bec3894
prakrutijoshi <joshiprakruti6@gmail.com>
2021-11-01 19:05:27 +0530

    final commit till day 6

--- JS/Node js/day3/assign1.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var list = require('./person');
-http.createServer(function (req, res) {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.write(JSON.stringify(list));
-        res.end();
-}).listen(3001);
